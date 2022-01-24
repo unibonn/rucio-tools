8ddd3ad16e706bc2512d3d1af85658ea872b2df9
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-01-24 22:20:08 +0100

    pleiades_quick: Drop auth, add dedicated "persistent" column.
    It seems authentication is not required anymore.

--- pleiades_quick.sh
+++ pleiades_quick.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl -s --capath /etc/grid-security/certificates --cacert $X509_USER_PROXY --cert $X509_USER_PROXY "https://rucio-hadoop.cern.ch/datasets_per_rse?rse=UNI-BONN_LOCALGROUPDISK" | \
+# Auth would require: --cacert $X509_USER_PROXY --cert $X509_USER_PROXY
+curl -s --capath /etc/grid-security/certificates "https://rucio-hadoop.cern.ch/datasets_per_rse?rse=UNI-BONN_LOCALGROUPDISK" | \
 	sort -k4 | \
-	awk '{SUM_TOT[$4]+=$5;if ($8 != "\"\"") {SUM_TMP[$4]+=$5}} END { for (ACC in SUM_TOT) { printf("%s;%.3f;%.3f\n",ACC,SUM_TOT[ACC]/1000/1000/1000/1000,SUM_TMP[ACC]/1000/1000/1000/1000) } }' | \
+	awk '{SUM_TOT[$4]+=$5;if ($8 != "\"\"") {SUM_TMP[$4]+=$5} else {SUM_PST[$4]+=$5}} END { for (ACC in SUM_TOT) { printf("%s;%.3f;%.3f;%.3f\n",ACC,SUM_TOT[ACC]/1000/1000/1000/1000,SUM_PST[ACC]/1000/1000/1000/1000,SUM_TMP[ACC]/1000/1000/1000/1000) } }' | \
 	LC_ALL=C sort -t ';' -k2,2 -g -r | \
-	column -s ';' -t -N 'Accounts,Space total,Space temporary'
+	column -s ';' -t -N 'Accounts,Space total,Space persistent,Space temporary'
