f646860f402b82009747964dc51c583c40247688
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-01-24 13:44:26 +0100

    pleiades_quick: Show "temporary" data (expiring rules) separately.
    See https://github.com/rucio/rucio/issues/4983 for the new naming.

--- pleiades_quick.sh
+++ pleiades_quick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -s --capath /etc/grid-security/certificates --cacert $X509_USER_PROXY --cert $X509_USER_PROXY "https://rucio-hadoop.cern.ch/datasets_per_rse?rse=UNI-BONN_LOCALGROUPDISK" | \
 	sort -k4 | \
-	awk '{SUM[$4]+=$5} END { for (ACC in SUM) { printf("%s;%.3f\n",ACC,SUM[ACC]/1000/1000/1000/1000) } }' | \
+	awk '{SUM_TOT[$4]+=$5;if ($8 != "\"\"") {SUM_TMP[$4]+=$5}} END { for (ACC in SUM_TOT) { printf("%s;%.3f;%.3f\n",ACC,SUM_TOT[ACC]/1000/1000/1000/1000,SUM_TMP[ACC]/1000/1000/1000/1000) } }' | \
 	LC_ALL=C sort -t ';' -k2,2 -g -r | \
-	column -s ';' -t -N 'Accounts,Space'
+	column -s ';' -t -N 'Accounts,Space total,Space temporary'
