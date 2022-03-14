1064a2aecee64e4cced7cba14eff2122081eb76d
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-03-14 11:50:20 +0100

    pleiades: Rucio dumps do not contain expiration date.
    Hence, the temporary / persistent state of datasets can not be deduced.
    So just show the space.

--- pleiades_csv.sh
+++ pleiades_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Auth would require: --cacert $X509_USER_PROXY --cert $X509_USER_PROXY
-echo "Accounts;Space persistent;Space temporary"
+echo "Accounts;Space usage"
 curl -s --capath /etc/grid-security/certificates "https://rucio-hadoop.cern.ch/datasets_per_rse?rse=UNI-BONN_LOCALGROUPDISK" | \
 	sort -k4 | \
-	awk '{SUM_TOT[$4]+=$5; if ($8 != "\"\"") {SUM_TMP[$4]+=$5} else {SUM_PST[$4]+=$5}} END { for (ACC in SUM_TOT) { printf("%s;%.3f;%.3f\n",ACC,SUM_PST[ACC]/1000/1000/1000/1000,SUM_TMP[ACC]/1000/1000/1000/1000) } }' | \
+	awk '{SUM_TOT[$4]+=$5} END { for (ACC in SUM_TOT) { printf("%s;%.3f\n",ACC,SUM_TOT[ACC]/1000/1000/1000/1000) } }' | \
 	LC_ALL=C sort -t ';' -k2,2 -g -r
