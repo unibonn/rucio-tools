34b3c8afeaafcd1659212c6048a2d391472cc8f6
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-03-24 23:16:15 +0100

    get_accs_usage2.sh: Fix ordering of results (by total usage).

--- get_accs_usage2.sh
+++ get_accs_usage2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./get_accs_usage2.py | \
 	egrep -v '^name' | \
-	sort -rn -k2 | \
+	sort -rn -k3 | \
 	awk -vFS=$'\t' 'BEGIN{OFS=","}{print $1, $2, $3/1000/1000/1000/1000, $4/1000/1000/1000/1000, $5/1000/1000/1000/1000}' | \
 	column -s, -t -N name,mail,size/TB,perm/TB,temp/TB
